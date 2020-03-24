88c98a72a9ecc9468c522f1476006ec090c156fa
SartoRiccardo <rikki.sarto@gmail.com>
2020-03-24 12:17:57 +0100

    Fixed transition error

--- src/components/ui/account/Avatar.js
+++ src/components/ui/account/Avatar.js
@@ -6,11 +6,11 @@ function Avatar(props) {
   const { image, active, className } = props;
 
   const disabledClass = active !== undefined && !active ? "avatar-disabled" : null;
-  
+
   const backgroundStyle = { backgroundImage: `url(${image})` };
   return (
-    <div className={`avatar-container ${className}`}>
-      <div className={`p-2 p-md-3 ${disabledClass}`}>
+    <div className={`avatar-container ${className || ""}`}>
+      <div className={`p-2 p-md-3 ${disabledClass || ""}`}>
         <div className="avatar bg-image grey lighten-2 w-100 p-100"
             style={backgroundStyle} />
       </div>
