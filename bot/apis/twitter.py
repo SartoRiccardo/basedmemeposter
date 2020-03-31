8c4cddbf2d834984f7a07542b5256ff600e76c1e
SartoRiccardo <rikki.sarto@gmail.com>
2020-03-31 19:46:01 +0200

    Conform to BasedPost

--- bot/apis/twitter.py
+++ bot/apis/twitter.py
@@ -1,8 +1,10 @@
-import twitter
 from config import config
+import apis.based
+
+import twitter
 
 
-def get_user_image_statuses(user):
+def userImageStatuses(user):
     api = twitter.Api(
         consumer_key=config("twitter", "consumer-key"),
         consumer_secret=config("twitter", "consumer-secret"),
@@ -16,8 +18,20 @@ def get_user_image_statuses(user):
         exclude_replies=True
     )
 
+    max_ratio = 16/9
+    min_ratio = 9/16
     ret = []
     for s in statuses:
-        if s.media:
-            ret.append(s)
+        if s.media and len(s.media) == 1 and s.media[0].type == "photo":
+            photo = s.media[0]
+            original_url = f"https://twitter.com/{user}/status/{s.id_str}"
+            ratio = None
+            for size in photo.sizes:
+                if size != "thumb":
+                    ratio = photo.sizes[size]["w"] / photo.sizes[size]["h"]
+
+            if ratio and min_ratio < ratio < max_ratio:
+                ret.append(apis.based.BasedPost(
+                    "twitter", s.id_str, original_url, photo.media_url_https
+                ))
     return ret
