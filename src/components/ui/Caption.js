03de3c57a5b01c26ee507119791e166cc590b324
SartoRiccardo <rikki.sarto@gmail.com>
2020-05-01 16:19:28 +0200

    Can copy caption

--- src/components/ui/Caption.js
+++ src/components/ui/Caption.js
@@ -2,18 +2,36 @@ import React from "react";
 import { MDBRow, MDBCol, MDBIcon } from "mdbreact";
 
 class Caption extends React.Component {
+  change = () => {
+
+  }
+
+  copy = () => {
+    const { caption } = this.props;
+    const textArea = document.getElementById(`caption-${caption.id}-textarea`);
+    textArea.focus();
+    textArea.select();
+    document.execCommand('copy');
+  }
+
+  delete = () => {
+
+  }
+
   render() {
     const { caption, className } = this.props;
     return (
       <MDBRow className="my-2 py-2 mx-1 grey lighten-3">
         <MDBCol className="break-word pr-0 text-justify" size="10">
-          <p className={className || ""}>{caption.text}</p>
+          <textarea id={`caption-${caption.id}-textarea`} className="hidden"
+              value={caption.text} readOnly />
+          <p id={`caption-${caption.id}`} className={className || ""}>{caption.text}</p>
         </MDBCol>
 
         <MDBCol className="text-right">
-          <MDBIcon className="icon-button" icon="pen" fixed /><br />
-          <MDBIcon className="icon-button" icon="copy" fixed /><br />
-          <MDBIcon className="icon-button" icon="trash" fixed />
+          <MDBIcon className="icon-button" icon="pen" onClick={this.change} fixed /><br />
+          <MDBIcon className="icon-button" icon="copy" onClick={this.copy} fixed /><br />
+          <MDBIcon className="icon-button" icon="trash" onClick={this.delete} fixed />
         </MDBCol>
       </MDBRow>
     );
