e7e3e0b82e3729f3fedd4e78f3c32048d8f4d765
SartoRiccardo <rikki.sarto@gmail.com>
2020-05-15 15:54:32 +0200

    Changed token key name to avoid conflicts

--- src/storage/session.js
+++ src/storage/session.js
@@ -1,14 +1,24 @@
 
+/**
+ * Reading about security turns out that:
+ * 1. You're not supposed to store access tokens in localStorage/sessionStorage
+ * 2. It's more secure to store a refresh token and use that to get a new one.
+ *
+ * This app is practically ready and it's a small-medium sized project so I'll
+ * leave it like this but I'll do the correct thing next time.
+ */
+
+const KEY_NAME = process.env.REACT_APP_TITLE + "_token";
 const storage = window.localStorage;
 
 export function getToken() {
-  return storage.getItem("token");
+  return storage.getItem(KEY_NAME);
 }
 
 export function setToken(value) {
-  storage.setItem("token", value);
+  storage.setItem(KEY_NAME, value);
 }
 
 export function deleteToken() {
-  storage.removeItem("token");
+  storage.removeItem(KEY_NAME);
 }
