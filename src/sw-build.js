c20c5c53e105875f7c84bb4d4b30ca65d1bb9740
SartoRiccardo <rikki.sarto@gmail.com>
2020-05-19 16:23:04 +0200

    Additional caching

--- src/sw-build.js
+++ src/sw-build.js
@@ -8,6 +8,16 @@ const buildSW = () => {
     globPatterns: [
       '**\/!(service-worker|precache-manifest.*).{js,css,html,png}',
     ],
+    additionalManifestEntries: [
+      {
+        url: "https://kit.fontawesome.com/d98896c0aa.js",
+        revision: null,
+      },
+      {
+        url: "https://fonts.googleapis.com/css2?family=Mukta:wght@400;700&display=swap",
+        revision: null,
+      },
+    ],
   });
 }
 buildSW();
