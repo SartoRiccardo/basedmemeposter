85bb05c8f4837c7bae174f9f0c492b93b7b0a6ae
SartoRiccardo <rikki.sarto@gmail.com>
2020-05-19 15:47:15 +0200

    Better logging UI

--- src/components/ui/log/SingleLog.js
+++ src/components/ui/log/SingleLog.js
@@ -4,8 +4,8 @@ import { MDBIcon } from "mdbreact";
 import moment from "moment";
 
 const levelStyling = {
-  debug: {background: "white", icon: "bug", color:"black"},
-  info: {background: "teal lighten-5", icon: "info-circle", color:"light-blue"},
+  debug: {background: "grey lighten-4", icon: "bug", color:"black"},
+  info: {background: "teal lighten-5", icon: "info", color:"light-blue"},
   warning: {background: "yellow lighten-4", icon: "exclamation-triangle", color:"amber"},
   error: {background: "deep-orange lighten-4", icon: "exclamation-circle", color:"red"},
   critical: {background: "red lighten-3", icon: "skull", color:"black"},
@@ -18,30 +18,36 @@ function SingleLog(props) {
   const dateText = moment.utc(date, "YYYY-MM-DD HH:mm:ss").fromNow();
 
   const username = account && (
-    <Link to={`/accounts/${account.id}`} className="black-text">
+    <Link to={`/accounts/${account.id}`} className="black-text text-uppercase">
       <u>{account.username}</u>
     </Link>
   );
 
+  const messageSection = level === "critical" ? (
+    <div className="stack-trace">
+      <code>
+        {message}
+      </code>
+    </div>
+  ) : ( <div>{message}</div> );
+
+  const py = username ? 2 : 1;
+
   return (
-    <div className={`${background} rounded-lg mt-1 px-3 py-1 clearfix`}>
-      <div className="d-block d-md-inline-block text-left">
+    <div className={`${background} rounded-lg mt-1 px-3 py-${py} clearfix`}>
+      <div className="text-left">
         <MDBIcon icon={icon} className={`mr-2 ${color}-text`} fixed />
 
-        <span className="text-uppercase mr-3">
-          {username}
+        <span className="mr-3">
+          {username || message}
         </span>
 
-        <span className="d-inline-block d-md-none float-right">
+        <span className="float-right">
           {dateText}
         </span>
       </div>
 
-      {message}
-
-      <div className="d-none d-md-inline-block text-right float-md-right">
-        {dateText}
-      </div>
+      {username && messageSection}
     </div>
   );
 }
